9f75b8ab72f491d3aa53264577e18505a6ec7cb1
gurjargaurav <78733671+gurjargaurav@users.noreply.github.com>
2021-04-16 18:56:36 +0530

    Home page html

--- New Html/js/custom.js
+++ New Html/js/custom.js
@@ -35,6 +35,33 @@ $(document).ready(function() {
 
 /*offering-slider*/
 
+/*footer-slider*/
+$(document).ready(function() {
+  $('.footer-slider .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: true,
+        margin: 20
+      }
+    }
+  })
+});
+
+/*footer-slider*/
+
 /*navbar scroll color change*/
 
 $(function () {
@@ -45,3 +72,4 @@ $(function () {
 });
 
 /*navbar scroll color change*/
+
